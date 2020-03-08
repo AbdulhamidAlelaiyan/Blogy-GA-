eba0df84afa593b8965415a7a2c55a8420ca57da
AbdulhamidAlelaiyan <quickisslow191@gmail.com>
2020-03-08 16:06:38 +0300

    Add articles routes

--- app/routes/articles.js
+++ app/routes/articles.js
@@ -27,4 +27,20 @@ router.post('/api/articles', (req, res) => {
         });
 });
 
+router.get('/api/articles/:id', (req, res) => {
+   Article.findById(req.params.id)
+       .then(article => {
+            if(article){
+                res.status(200).json({article});
+            } else {
+                res.status(404).json({error: {
+                    name: 'Document not found',
+                    message: 'The provided ID doesn\'t match any documents',
+                    }});
+            }
+       })
+       .catch(error => {
+           res.status(500).json({error})
+       });
+});
 module.exports = router;
