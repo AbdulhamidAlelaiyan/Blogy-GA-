c2c97954a4813d41888c8fc5f9e1c4618871ff3c
AbdulhamidAlelaiyan <quickisslow191@gmail.com>
2020-03-08 11:13:33 +0300

    rearrange directories

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const indexRouter = require('./app/routes/index');
+
 const app = express();
 
+
 const port = process.env.PORT || 5000;
+
+app.use(indexRouter);
 
 app.listen(port, () => {
    console.log(`Blogy is listening on port ${port}`);
